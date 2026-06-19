2bcabecfe22ae84131ad27518f42cf02cd814df9
aish <aishwaryamanojmc@gmail.com>
2026-06-19 16:59:58 +0530

    Time: 14 ms (38.57%), Space: 27.5 MB (51.17%) - LeetHub

---
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int>ans;
+        vector<int>freq(n*n+1);
+
+        for(auto num:grid) {
+            for(int val:num){
+                freq[val]++;
+            }
+        }
+
+
+        int repeated = -1;
+        int missing = -1;
+        for(int i = 1;i<n*n+1;i++) {
+            if(freq[i] == 2) {
+                repeated = i;
+            }
+
+            if(freq[i] == 0) {
+                missing = i;
+            }
+
+        }
+        ans.push_back(repeated);
+        ans.push_back(missing);
+
+        return ans;
+        
+    }
+};
