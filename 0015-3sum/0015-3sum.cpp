4fb8a5b9065eabae2d35c0c6dd769d0835f30216
aish <aishwaryamanojmc@gmail.com>
2026-06-25 11:31:09 +0530

    Time: 45 ms (71.87%), Space: 29.3 MB (30.41%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -16,17 +16,19 @@ public:
                     while(left < right && nums[left] == nums[left-1]) {
                         left++;
                     }
-                    while(left < right && nums[right]==nums[right+1]) {
+                    while(left<right && nums[right] == nums[right+1]) {
                         right--;
                     }
-                } else if(sum < 0) {
-                    left ++;
-                }else {
+                } else if (sum < 0 ) {
+                    left++;
+                } else {
                     right--;
-                } 
+                }
             }
+
         }
 
         return ans;
+        
     }
 };
