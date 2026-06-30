ec7b5da0aab2659becdb2528305a7afec1feb010
aish <aishwaryamanojmc@gmail.com>
2026-06-30 16:58:34 +0530

    Time: 3 ms (89.15%), Space: 23.7 MB (82.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,15 +5,11 @@ public:
         vector<vector<int>>ans;
         ans.push_back(intervals[0]);
         for(int i=1;i<intervals.size();i++) {
-            int first = intervals[i][0];
-            int last = intervals[i][1];
-            if(first<=ans.back()[1]) {
-                ans.back()[1] = max(ans.back()[1],last);
-            }
-            else {
+            if(intervals[i][0] <= ans.back()[1]) {
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+            } else  {
                 ans.push_back(intervals[i]);
             }
-            
         }
         return ans;
     }
