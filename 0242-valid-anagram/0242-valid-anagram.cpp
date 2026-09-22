555789e7d7d44d4493b4e041cdc21485c71a2846
aish <aishwaryamanojmc@gmail.com>
2026-09-22 06:59:48 +0530

    Time: 3 ms (44.92%), Space: 9.9 MB (25.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+
+        if(s.length()!=t.length()) return false;
         unordered_map<char,int>freq;
         for(char c:s) {
             freq[c]++;
